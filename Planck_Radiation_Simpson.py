e1a7c8c2a4e0fa20ef887f0af8bbb5c4f9afe149
Alvandhyto Putra <alvandhyto@gmail.com>
2023-01-14 12:34:28 +0700

    Update Planck_Radiation_Simpson.py

--- Planck_Radiation_Simpson.py
+++ Planck_Radiation_Simpson.py
@@ -1,18 +1,46 @@
 import numpy as np
+from sympy import symbols
 
-a = 1000
-b = 1010
-n = int(input("Banyaknya partisi :"))
+np.seterr(divide='ignore', invalid='ignore')
+
+#CONSTANTS
+h = 6.6256*(10**(-27))
+c = 2.99792*(10**10)
+k = 1.3805*(10**(-16))
+pi = np.pi
+exp = np.exp
+T = [10, 100, 1000]
+
+#INPUT JUMLAH PARTISI
+dl_arr = []
+interval = ([0, 10], [100, 110], [1000, 1010], [0, np.inf])
+n = int(input("Input jumlah partisi: "))
 while (n % 2 != 0):
- print("Silakan masukkan angka genap")
- n = int(input("Banyaknya partisi :"))
+    print("Jumlah partisi harus merupakan bilangan genap!")
+    n = int(input("Input jumlah partisi: "))
+
+#PERHITUNGAN ATURAN SIMPSON
 else:
- h = (b - a) / (n - 1)
- x = np.linspace(a, b, n)
- f = np.sin(x)
- 
- I_simp = (h/3) * (f[0] + 2*sum(f[:n-2:2]) + 4*sum(f[1:n-1:2]) + f[n-1])
- galat_simp = 2 - I_simp
- 
- print(I_simp)
- print(galat_simp)
+    f_arr = [[] for x in interval]
+
+    for i in range(len(interval)):
+        dl = np.linspace(interval[i][0], interval[i][1], n)
+        dl_arr.append(dl)
+
+    print("\nPERHITUNGAN ATURAN SIMPSON")
+
+    for i in range(len(interval)):
+        for j in range(len(T)):
+            f = (2*pi*h*(c**2)) / ((dl_arr[i]**(5))*(exp((h*c)/(k*dl_arr[i]*T[j]))-1))
+            f_arr[i].append(f)
+
+    for i in range(len(interval)):
+        print("\nInterval \u03BB {}".format(str(interval[i])))
+        
+        for j in range(len(T)):
+            H = (interval[i][1] - interval[i][0]) / (n - 1)
+            I = (H/3)*(f_arr[i][j][0] + 2*sum(f_arr[i][j][:n-2:2]) + 4*sum(f_arr[i][j][1:n-1:2]) + f_arr[i][j][n-1])
+            err = 2 - I
+
+            print("\nEnergi untuk T = {:d}K: {}".format(T[j], I))
+            print("Galat iterasi untuk T = {:d}K: {}".format(T[j], err))
