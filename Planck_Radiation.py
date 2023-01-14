0395b37f7c9fde377879b9ab6a7b4d6d11b7e717
Alvandhyto Putra <alvandhyto@gmail.com>
2023-01-14 12:07:57 +0700

    Update Planck_Radiation.py

--- Planck_Radiation.py
+++ Planck_Radiation.py
@@ -13,26 +13,29 @@ T = [10, 100, 1000]
 
 #INPUT JUMLAH PARTISI
 dl_arr = []
-interval = ([0, 10], [100, 110], [1000, 1010])
-n = int(input('Input Jumlah Partisi: '))
+interval = ([0, 10], [100, 110], [1000, 1010], [0, np.inf])
+n = int(input('Input jumlah partisi: '))
 
 for i in range(len(interval)):
-    H = (interval[i][1] - interval[i][0]) / (n - 1)
     dl = np.linspace(interval[i][0], interval[i][1], n)
     dl_arr.append(dl)
 
 #PERHITUNGAN ATURAN TRAPESIUM
-f_arr = [[],[],[]]
+f_arr = [[] for x in interval]
+print("\nPERHITUNGAN ATURAN TRAPESIUM")
 
-for i in range(len(dl_arr)):
+for i in range(len(f_arr)):
     for j in range(len(T)):
         f = (2*pi*h*(c**2)) / ((dl_arr[i]**(5))*(exp((h*c)/(k*dl_arr[i]*T[j]))-1))
         f_arr[i].append(f)
 
-for i in range(len(dl_arr)):
-    for j in range(len(f_arr)):
-        I_trap = (H/2)*(f_arr[i][j][0] + 2 * sum(f_arr[i][j][1:n-1]) + f_arr[i][j][n-1])
-        err_trap = 2 - I_trap
+for i in range(len(f_arr)):
+    print("\nInterval \u03BB {}".format(str(interval[i])))
+    
+    for j in range(len(T)):
+        H = (interval[i][1] - interval[i][0]) / (n - 1)
+        I = (H/2)*(f_arr[i][j][0] + (2*sum(f_arr[i][j][1:n-1])) + f_arr[i][j][n-1])
+        err = 2 - I
 
-        print("\nEnergi untuk interval {} dan T = {:d}K: {}".format(str(interval[i]), T[j], I_trap))
-        print("Galat iterasi interval {} dan T = {:d}K: {}".format(str(interval[i]), T[j], err_trap))
+        print("\nEnergi untuk T = {:d}K: {}".format(T[j], I))
+        print("Galat iterasi untuk T = {:d}K: {}".format(T[j], err))
