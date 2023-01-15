8a571d1c2be58d8450198e08f8f22522499ff57f
Alvandhyto Putra <alvandhyto@gmail.com>
2023-01-15 20:09:02 +0700

    Update Planck_Radiation_X.py

--- Planck_Radiation.py
+++ Planck_Radiation.py
@@ -1,12 +1,18 @@
+import math
 import os
 import numpy as np
+import warnings
 from sympy import symbols
-from time import sleep
+from scipy.special.orthogonal import p_roots
+from scipy.integrate import quad
+from sympy import *
 
-np.seterr(divide='ignore', invalid='ignore')
+#np.seterr(divide='ignore', invalid='ignore')
+warnings.filterwarnings("ignore")
 os.system("cls")
 
 #KONSTANTA
+i = 0
 h = 6.6256*(10**(-27))
 c = 2.99792*(10**10)
 k = 1.3805*(10**(-16))
@@ -20,6 +26,7 @@ interval = ([0, 10], [100, 110], [1000, 1010], [0, inf])
 print("KALKULATOR ENERGI RADIASI")
 print("1. Metode Aturan Trapesium")
 print("2. Metode Aturan Simpson")
+print("3. Metode Aturan Gauss-Legendre")
 option = int(input("Input metode yang diinginkan: "))
 
 #METODE ATURAN TRAPESIUM
@@ -27,30 +34,68 @@ if (option == 1):
     os.system("cls")
     print("KALKULATOR ENERGI RADIASI MENGGUNAKAN METODE TRAPESIUM")
 
+    #INISIALISASI
+    x = Symbol('x')
+    eksak_list = []
+
     #INPUT JUMLAH PARTISI
     dl_arr = []
+    n = int(input('\nInput jumlah partisi: '))
 
-    n = int(input("\nInput jumlah partisi: "))
     for i in range(len(interval)):
-        dl = np.linspace(interval[i][0], interval[i][1], n)
+        if i == 0:
+            dl = np.linspace(interval[i][0], interval[i][1], n)
+            def g(x):
+                    return (2*pi*h*(c**2))/(x**5*(exp(h*c/k*x*T[i])-1))
+            eksak = quad(g, interval[i][0], interval[i][1])[0]
+            eksak_list.append(eksak)
+        
+        else:
+            dl = np.linspace(interval[i][0], interval[i][1], n+1)
+            
+            if i == 3:
+                dl = np.linspace(interval[i][0], interval[i][1], n)
+                eksak_list.append(0)
+                dl_arr.append(dl)
+            
+            else:
+                def g(x):
+                        return (2*pi*h*(c**2))/(x**5*(exp(h*c/k*x*T[i])-1))
+                eksak = quad(g, interval[i][0], interval[i][1])[0]
+                eksak_list.append(eksak)
+        
         dl_arr.append(dl)
 
     #PERHITUNGAN ATURAN TRAPESIUM
     f_arr = [[] for x in interval]
-    print("\nHASIL PERHITUNGAN ATURAN TRAPESIUM")
+    print("\nPERHITUNGAN ATURAN TRAPESIUM")
 
     for i in range(len(interval)):
         for j in range(len(T)):
-            f = (2*pi*h*(c**2)) / ((dl_arr[i]**(5))*(exp((h*c)/(k*dl_arr[i]*T[j]))-1))
+            if interval[i][0] == 0:
+                f = (2*pi*h*(c*2)) / ((dl_arr[i]**(5))*(exp((h*c)/(k*dl_arr[i]*T[j]))-1))
+                
+            else:
+                f = (2*pi*h*(c*2)) / ((dl_arr[i][1:]**(5))*(exp((h*c)/(k*dl_arr[i][1:]*T[j]))-1))
             f_arr[i].append(f)
 
     for i in range(len(interval)):
         print("\nInterval \u03BB {}".format(str(interval[i])))
         
         for j in range(len(T)):
-            H = (interval[i][1] - interval[i][0]) / (n - 1)
-            I = (H/2)*(f_arr[i][j][0] + (2*sum(f_arr[i][j][1:n-1])) + f_arr[i][j][n-1])
-            err = 2 - I
+            if interval[i][0] == 0:
+                H = (interval[i][1] - interval[i][0]) / (n - 1)
+                I = (H/2)*(f_arr[i][j][0] + (2*sum(f_arr[i][j][1:n-1])) + f_arr[i][j][n-1])
+        
+            else:
+                H = (interval[i][1] - interval[i][0]) / n
+                I = (H/2)*(f_arr[i][j][0] + (2*sum(f_arr[i][j][1:])) + f_arr[i][j][n-1])
+        
+            err = eksak_list[j] - I
+        
+            if math. isnan(I) == True:
+                I = 0
+                err = eksak_list[j]
 
             print("\nEnergi untuk T = {:d}K: {}".format(T[j], I))
             print("Galat iterasi untuk T = {:d}K: {}".format(T[j], err))
@@ -60,8 +105,12 @@ elif (option == 2):
     os.system("cls")
     print("KALKULATOR ENERGI RADIASI MENGGUNAKAN METODE SIMPSON")
 
-    #INPUT JUMLAH PARTISI
+    #INISIALISASI
+    x = Symbol('x')
+    eksak_list = []
     dl_arr = []
+
+    #INPUT JUMLAH PARTISI
     n = int(input("\nInput jumlah partisi: "))
     while (n % 2 != 0):
         print("Jumlah partisi harus merupakan bilangan genap!")
@@ -72,10 +121,21 @@ elif (option == 2):
         f_arr = [[] for x in interval]
 
         for i in range(len(interval)):
-            dl = np.linspace(interval[i][0], interval[i][1], n)
-            dl_arr.append(dl)
-
-        print("\nHASIL PERHITUNGAN ATURAN SIMPSON")
+            if interval[i][1] < 99999999:
+                def g(x):
+                    return (2*pi*h*(c**2))/(x**5*(exp(h*c/k*x*T[i])-1))
+          
+                dl = np.linspace(interval[i][0], interval[i][1], n)
+                eksak = quad(g, interval[i][0], interval[i][1])[0]
+                eksak_list.append(eksak)
+                dl_arr.append(dl)
+          
+            else:
+                dl = np.linspace(interval[i][0], interval[i][1], n)
+                eksak_list.append(0)
+                dl_arr.append(dl)
+        
+        print("\nPERHITUNGAN ATURAN SIMPSON")
 
         for i in range(len(interval)):
             for j in range(len(T)):
@@ -88,10 +148,53 @@ elif (option == 2):
             for j in range(len(T)):
                 H = (interval[i][1] - interval[i][0]) / (n - 1)
                 I = (H/3)*(f_arr[i][j][0] + 2*sum(f_arr[i][j][:n-2:2]) + 4*sum(f_arr[i][j][1:n-1:2]) + f_arr[i][j][n-1])
-                err = 2 - I
+                err = eksak_list[j] - I
+                
+                if math. isnan(I) == True:
+                    I = 0
+                    err = eksak_list[j]
 
                 print("\nEnergi untuk T = {:d}K: {}".format(T[j], I))
                 print("Galat iterasi untuk T = {:d}K: {}".format(T[j], err))
+
+#METODE ATURAN GAUSS-LEGENDRE
+elif (option == 3):
+    os.system("cls")
+    print("KALKULATOR ENERGI RADIASI MENGGUNAKAN METODE GAUSS-LEGENDRE")
+
+    def f(x):
+        return (2*np.pi*h*(c**2))/(x**5*(np.exp(h*c/k*x*T)-1))
+        
+    n = int(input("\nInput jumlah titik interpolasi: "))
+
+    print("\nHASIL PERHITUNGAN ATURAN GAUSS-LEGENDRE")
+
+    for T in T:
+        print("\n========")
+        print('T: ', T)
+        print("========")
+        
+        for a, b in [[0, 10], [100, 110], [1000, 1010], [0, inf]] :
+            print(f"\nInterval ({a}, {b})")
+            
+            i += 1
+            dl = np.linspace(a, b, n+1)
+            [x, w] = p_roots(n+1)
+            
+            I = 0.5*(b-a)*sum(w*f(0.5*(b-a)*x+0.5*(b+a)))
+            
+            if i == 3:
+                err = 0 - I
+            
+            else:
+                err = quad(f, a, b)[0] - I
+            
+            if math.isnan(I) == True:
+                    I = 0
+                    err = 0 - I
+            
+            print('Nilai: ', I)
+            print('Error: ', err)
 
 else:
     print("Pilihan tidak terdapat di menu, silahkan ulangi.")
--- test.py
+++ test.py
@@ -1,146 +1,66 @@
 import numpy as np
-import pandas
+from sympy import symbols
+import math
+from scipy.integrate import quad
+from sympy import *
 
-#KONSTANTA
-V = 15
-R = 4.7
-L = 50*(10**(-3))
-C = 10**(-3)
-x = 0
-y = V/R
-z = 0
-h = 0.002
-dt = 0.1
+np.seterr(divide='ignore', invalid='ignore')
 
-print("========================================")
-print("Pilih metode perhitungan yang diinginkan")
+#CONSTANTS
+h = 6.6256*(10**(-27))
+c = 2.99792*(10**10)
+k = 1.3805*(10**(-16))
+pi = np.pi
+exp = np.exp
+inf = np.inf
+T = [10, 100, 1000]
+x = Symbol('x')
 
-bo = True
-while bo == True:
-    rules = input('\n1. Metode Euler \n2. Metode Heun \n3. Metode Runge Kutta Orde 4 \n\nPilihan: ')
-    
-    if rules == "1" or rules == "2" or rules == "3":
-        bo = False
-    
-    else:
-        print("Silahkan masukkan angka 1, 2, atau 3 saja")
-        bo = True
+
+eksak_list = []
+#INPUT JUMLAH PARTISI
+dl_arr = []
+interval = ([0, 10], [100, 110], [1000, 1010], [0, inf])
+n = int(input("Input jumlah partisi: "))
+while (n % 2 != 0):
+    print("Jumlah partisi harus merupakan bilangan genap!")
+    n = int(input("Input jumlah partisi: "))
+
+#PERHITUNGAN ATURAN SIMPSON
+else:
+    f_arr = [[] for x in interval]
+
+    for i in range(len(interval)):
+        if interval[i][1] < 99999999:
+            def g(x):
+                return (2*pi*h*(c**2))/(x**5*(exp(h*c/k*x*T[i])-1))
+            dl = np.linspace(interval[i][0], interval[i][1], n)
+            eksak = quad(g, interval[i][0], interval[i][1])[0]
+            eksak_list.append(eksak)
+            dl_arr.append(dl)
+        else:
+            dl = np.linspace(interval[i][0], interval[i][1], n)
+            eksak_list.append(0)
+            dl_arr.append(dl)
+    print("\nPERHITUNGAN ATURAN SIMPSON")
+
+    for i in range(len(interval)):
+        for j in range(len(T)):
+            f = (2*pi*h*(c**2)) / ((dl_arr[i]**(5))*(exp((h*c)/(k*dl_arr[i]*T[j]))-1))
+            
+            f_arr[i].append(f)
+
+    for i in range(len(interval)):
+        print("\nInterval \u03BB {}".format(str(interval[i])))
         
-if rules == "1":
-    partisi = int(dt/h)     #int biar positif
-    A = [[0 for x in range(partisi + 1)] for y in range (3)]
+        for j in range(len(T)):
+            H = (interval[i][1] - interval[i][0]) / (n - 1)
+            I = (H/3)*(f_arr[i][j][0] + 2*sum(f_arr[i][j][:n-2:2]) + 4*sum(f_arr[i][j][1:n-1:2]) + f_arr[i][j][n-1])
+            err = eksak_list[j] - I
+            
+            if math. isnan(I) == True:
+                I = 0
+                err = eksak_list[j]
 
-    #Mengenai Nilai awal
-    A[0][0] = x
-    A[1][0] = y
-    A[2][0] = x*y/C
-
-    for i in range(partisi):
-        #Slope nya
-        s = z 
-        j = (-R*z-y/C)/L
-        
-        #Untuk iterasi selanjutnya
-        yn = y + h*s
-        zn = z + h*j
-        
-        #Mempersiapkan iterasi selanjurnya+store hasil
-        y = yn
-        z = zn
-        x += h
-
-        A[0][i+1] = x
-        A[1][i+1] = yn
-        
-        #Mencari nilai v untuk iterasi i+1 atau untuk t=(i+1)*0,002
-        A[2][i+1] = A[0][i+1] * A[1][i+1] / C
-
-    df = {'t' : A[0], 'I' : A[1], 'V_c' : A[2]}
-    
-    print("==============================")
-    print("Hasil perhitungan Metode Euler\n")
-    print(pandas.DataFrame(data = df))
-
-if rules == "2":
-    partisi = int(dt/h)    #int biar positif
-    A = [[0 for x in range(partisi + 1)] for y in range (3)]
-    
-    #Mengenai Nilai awal
-    A[0][0] = x
-    A[1][0] = y
-    A[2][0] = x*y/C
-    
-    for i in range(0,partisi):
-        #Slope
-        s = z #s=slope 
-        j = (-R*z-y/C)/L
-        
-        #Untuk iterasi selanjutnya
-        #Prediktor
-        yn = y + h*s
-        zn = z + h*j
-        
-        #Korektor
-        y1 = y + h/2*(z + zn)
-        z1 = z + h/2*(((-R*z-y/C)/L) + ((-R*zn-yn/C)/L))
-        
-        #Mempersiapkan iterasi selanjurnya+store hasil
-        y = y1
-        z = z1
-        x += h
-        A[0][i+1] = x
-        A[1][i+1] = y1
-        
-        #Mencari nilai v untuk iterasi i+1 atau untuk t=(i+1)*0,002
-        A[2][i+1] = A[0][i+1] * A[1][i+1] / C
-
-    df = {'t' : A[0], 'I' : A[1], 'V_c' : A[2]}
-    
-    print("=============================")
-    print("Hasil perhitungan Metode Heun\n")
-    print(pandas.DataFrame(data = df))
-
-if rules == "3":
-    partisi = int(dt/h)
-    A = [[0 for x in range(partisi + 1)] for y in range (3)]
-    
-    #Mengenai Nilai awal
-    A[0][0] = x
-    A[1][0] = y
-    A[2][0] = x*y/C
-    
-    for i in range(0,partisi):
-        # Mencari nilai k1-k4 dan l1-l4
-        k1 = z
-        l1 = (-R*z-y/C)/L
-        k2 = (z+l1/2*h)
-        l2 = (-R*(z+l1/2*h)-(y+k1/2*h)/C)/L 
-        k3 = (z+l2/2*h)
-        l3 = (-R*(z+l2/2*h)-(y+k2/2*h)/C)/L
-        k4 = (z+l3*h)
-        l4 = (-R*(z+l3*h)-(y+k3*h)/C)/L
-    
-        #Faktor penambah diiterasi adalah
-        kh = (k1+ 2*k2 + 2*k3 + k4)/6*h #untuk fungsi f
-        lh = (l1+ 2*l2 + 2*l3 + l4)/6*h
-    
-        #Nilai y dan z di iterasi berikutnya:
-        yn = y + kh
-        zn = z + lh
-    
-        #Mempersiapkan iterasi selanjurnya+store hasil
-        x += h
-        y = yn
-        z = zn
-        
-        A[0][i+1] = x
-        A[1][i+1] = yn
-    
-        #Mencari nilai v untuk iterasi i+1 atau untuk t=(i+1)*0,002
-        A[2][i+1] = A[0][i+1] * A[1][i+1] / C
-
-    df = {'t' : A[0], 'I' : A[1], 'V_c' : A[2]}
-    
-    print("===========================================")
-    print("Hasil perhitungan Metode Runge Kutta Orde 4\n")
-    print(pandas.DataFrame(data = df))
+            print("\nEnergi untuk T = {:d}K: {}".format(T[j], I))
+            print("Galat iterasi untuk T = {:d}K: {}".format(T[j], err))
